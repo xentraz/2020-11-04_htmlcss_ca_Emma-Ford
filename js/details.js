4ab453bf20471feb5f0b01eeaf4761443aea7c03
Emma <69841693+xentraz@users.noreply.github.com>
2021-04-25 17:24:02 +0200

    styled detail update

--- js/details.js
+++ js/details.js
@@ -16,15 +16,50 @@ async function getGames(){
     document.querySelector('.gameInfo').innerHTML += 
     `
     <div class="gameDetails">
-    <h1>${gamesResult.name}</h1>
-    <div class="gameImg"><img src="${gamesResult.images[0].src}"/></div>
-    <p>${gamesResult.description}</p>
-    <p>${gamesResult.categories[0].name}</p>
-    <p>${gamesResult.categories[1].name}</p>
-    <p>In Stock: ${gamesResult.is_in_stock}</p>
-    <p>Price: ${gamesResult.prices.price}-, NOK</p>
+      <div class="gameName">
+        <h1>${gamesResult.name}</h1>
+      </div>
+      <div class="gameImg2"><img src="${gamesResult.images[0].src}"/></div>
+      <div class="gameDesc">
+        <p>${gamesResult.description}</p>
+      </div>
     </div>
     `;
+  
+    document.querySelector('.stockStatus').innerHTML += 
+    `
+    <p>In Stock: ${gamesResult.is_in_stock}</p>
+    `; 
+  
+    document.querySelector('.tags').innerHTML += 
+    `
+    <div class="tags">
+    <p>Available On: ${gamesResult.tags[0].name}, ${gamesResult.tags[1].name}, ${gamesResult.tags[2].name}</p>
+    </div>
+    `; 
+
+    if (gamesResult.on_sale) {
+      document.querySelector('.sale').innerHTML += 
+    `
+    <p>On Sale For: ${gamesResult.prices.sale_price}-, NOK</p>
+    <p class="origionalPrice">Origional Price: ${gamesResult.prices.regular_price}-,NOK</p>
+    `; 
+    } else {
+      document.querySelector('.sale').style.display = 'none';
+    }
+
+    document.querySelector('.basketButton').innerHTML += 
+    `
+    <div class="priceButton">
+    <p>Price: ${gamesResult.prices.price}-, NOK</p>
+    </div>
+    `; 
+
+    document.querySelector('.categories').innerHTML += 
+    `
+    <div class="categoriesList">
+    <p>Category: ${gamesResult.categories[0].name}, ${gamesResult.categories[1].name}</p>`; 
+
   } catch (error) {
     document.querySelector('.alert').innerHTML = showAlertToUser (
       'An error occured',
